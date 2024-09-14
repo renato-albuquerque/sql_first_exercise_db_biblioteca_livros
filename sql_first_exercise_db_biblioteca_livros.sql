d3f96aea7a2465fbe5dc3c2b422c4658110de2e7
renato albuquerque <renatomalb@hotmail.com>
2024-09-14 20:34:10 -0300

    updated sql file

--- sql_first_exercise_db_biblioteca_livros.sql
+++ sql_first_exercise_db_biblioteca_livros.sql
@@ -4,26 +4,26 @@
 
 -- 2ª etapa, criar a tabela: livros.
 
--- CREATE TABLE IF NOT EXISTS public.livros (
--- 	id serial NOT NULL,
--- 	titulo character varying (100),
--- 	autor character varying (100),
--- 	ano_publicacao integer,
--- 	genero character varying,
--- 	CONSTRAINT livros_pkey PRIMARY KEY (id)
--- );
+CREATE TABLE IF NOT EXISTS public.livros (
+ 	id serial NOT NULL,
+ 	titulo character varying (100),
+ 	autor character varying (100),
+ 	ano_publicacao integer,
+ 	genero character varying,
+ 	CONSTRAINT livros_pkey PRIMARY KEY (id)
+);
 
 -- Checar as colunas e tipos de dados que foram criados.
--- SELECT * FROM public.livros;
+SELECT * FROM public.livros;
 
 -- 3ª etapa, inserir valores na tabela: livros.
 
--- INSERT INTO public.livros (titulo, autor, ano_publicacao, genero) VALUES 
--- ('Dom Quixote', 'Miguel de Cervantes', 1605, 'Romance'),
--- ('1984', 'George Orwell', 1949, 'Ficção Distópica'),
--- ('O Senhor dos Anéis', 'J.R.R. Tolkien', 1954, 'Fantasia'),
--- ('Crime e Castigo', 'Fiódor Dostoiévski', 1866, 'Romance'),
--- ('Orgulho e Preconceito', 'Jane Austen', 1813, 'Romance');
+INSERT INTO public.livros (titulo, autor, ano_publicacao, genero) VALUES 
+('Dom Quixote', 'Miguel de Cervantes', 1605, 'Romance'),
+('1984', 'George Orwell', 1949, 'Ficção Distópica'),
+('O Senhor dos Anéis', 'J.R.R. Tolkien', 1954, 'Fantasia'),
+('Crime e Castigo', 'Fiódor Dostoiévski', 1866, 'Romance'),
+('Orgulho e Preconceito', 'Jane Austen', 1813, 'Romance');
 
 -- Checar valores inseridos na tabela: livros.
--- SELECT * FROM public.livros;
+SELECT * FROM public.livros;
